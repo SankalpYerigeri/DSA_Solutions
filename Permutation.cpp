b90c9c284dd14d6699babe3c2e97e1a310d2f476
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-11 20:20:04 +0530

    Permutation A-1

---
+++ Permutation.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void recurPermute(vector<int>& ds, vector<vector<int>>& ans, vector<int>& nums, int freq[])
+    {
+        if(ds.size()==nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(!freq[i])
+            {
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                recurPermute(ds, ans, nums, freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+
+vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int freq[nums.size()];
+        for(int i=0; i<nums.size(); i++) freq[i]=0;
+        recurPermute(ds, ans, nums, freq);
+        return ans;
+    }
+
+int main()
+{
+    vector<int> nums{1,2,3};
+    permute(nums);
+
+}
